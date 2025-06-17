50945e86db74166e33de28519cc0f18cffcce503
David Victoria <davidvictoria64@gmail.com>
2025-06-17 11:14:10 -0600

    feat: ✨ add assets and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,11 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "---",
+  description:
+    "A community-driven platform asking and answering questions about software development. Get help, share knowledge, and connect with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures and more.",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
